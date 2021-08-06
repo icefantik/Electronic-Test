f1f6c812ff371b548c697f4745453b57d4454e3d
petya <mityushin_petya@mail.ru>
2021-08-06 14:51:45 +0300

    work on read from xml file

--- app/src/main/java/com/parse/xmlfile/ParseXmlFile.java
+++ app/src/main/java/com/parse/xmlfile/ParseXmlFile.java
@@ -20,46 +20,54 @@ public class ParseXmlFile
         ArrayList<String> questions = new ArrayList<String>();
         TestItems testItemsElems = new TestItems();
         ArrayList <TestItems> listTestItem = new ArrayList<TestItems>();
-            while (parser.next() != XmlPullParser.END_DOCUMENT)
-            {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
-                }
-                if (parser.getName().contains("question")) {
-                    parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
-                    if (parser.next() == XmlPullParser.TEXT) {
-                        questions.add(parser.getText());
-                        parser.nextTag();
-                    }
-                    parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
-                }
-                else if (parser.getName().equals("coundQuestions")) {
-                    parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
-                    if (parser.next() == XmlPullParser.TEXT) {
-                        testItemsElems.CountQuestions = Integer.parseInt(parser.getText());
-                        parser.nextTag();
-                    }
-                    parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
-                }
-                else if (parser.getName().equals("numAnswer")) {
-                    parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
-                    if (parser.next() == XmlPullParser.TEXT) {
-                        testItemsElems.NumAnswer = Integer.parseInt(parser.getText());
-                        parser.nextTag();
-                    }
-                    parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
-                }
-                else if (parser.getName().equals("kindButton")) {
-                    parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
-                    if (parser.next() == XmlPullParser.TEXT) {
-                        testItemsElems.KindAnswer = Integer.parseInt(parser.getText());
-                        parser.nextTag();
-                    }
-                    parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
-                    //listTestItem.add(testItemsElems);
-                }
+        while (parser.next() != XmlPullParser.END_DOCUMENT)
+        {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
             }
-
+            String nameTage = parser.getName();
+            if (parser.getName().contains("question")) {
+                parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
+                if (parser.next() == XmlPullParser.TEXT) {
+                    questions.add(parser.getText());
+                    parser.nextTag();
+                }
+                parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
+            }
+            else if (parser.getName().equals("coundQuestions")) {
+                parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
+                if (parser.next() == XmlPullParser.TEXT) {
+                    testItemsElems.CountQuestions = Integer.parseInt(parser.getText());
+                    parser.nextTag();
+                }
+                parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
+            }
+            else if (parser.getName().equals("numAnswer")) {
+                parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
+                if (parser.next() == XmlPullParser.TEXT) {
+                    testItemsElems.NumAnswer = Integer.parseInt(parser.getText());
+                    parser.nextTag();
+                }
+                parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
+            }
+            else if (parser.getName().equals("kindButton")) {
+                parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
+                if (parser.next() == XmlPullParser.TEXT) {
+                    testItemsElems.KindAnswer = Integer.parseInt(parser.getText());
+                    parser.nextTag();
+                }
+                parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
+            }
+            else if (parser.next() != XmlPullParser.END_DOCUMENT) {
+                testItemsElems.questions = questions;
+                listTestItem.add(testItemsElems);
+                testItemsElems = new TestItems();
+                questions = new ArrayList<String>();
+                String nameTage2 = parser.getName();
+            }
+        }
+        testItemsElems.questions = questions;
+        listTestItem.add(testItemsElems);
         return listTestItem;
     }
 
