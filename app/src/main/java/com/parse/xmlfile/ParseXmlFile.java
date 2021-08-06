7123c32ddf42be9b334acc3173bde26d356f61f8
petya <mityushin_petya@mail.ru>
2021-08-06 19:59:50 +0300

    Make read from file

--- app/src/main/java/com/parse/xmlfile/ParseXmlFile.java
+++ app/src/main/java/com/parse/xmlfile/ParseXmlFile.java
@@ -17,15 +17,17 @@ public class ParseXmlFile
 {
     public ArrayList <TestItems> parsXmlFile(XmlPullParser parser) throws IOException, XmlPullParserException
     {
+        //XmlPullParser.END_TAG
         ArrayList<String> questions = new ArrayList<String>();
         TestItems testItemsElems = new TestItems();
         ArrayList <TestItems> listTestItem = new ArrayList<TestItems>();
         while (parser.next() != XmlPullParser.END_DOCUMENT)
         {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
+            int a = parser.getEventType();
+            String nameTage = parser.getName();
+            if (parser.getEventType() != XmlPullParser.START_TAG && parser.getEventType() != XmlPullParser.END_TAG) {
                 continue;
             }
-            String nameTage = parser.getName();
             if (parser.getName().contains("question")) {
                 parser.require(XmlPullParser.START_TAG, parser.getNamespace(), parser.getName());
                 if (parser.next() == XmlPullParser.TEXT) {
@@ -58,7 +60,7 @@ public class ParseXmlFile
                 }
                 parser.require(XmlPullParser.END_TAG, parser.getNamespace(), parser.getName());
             }
-            else if (parser.next() != XmlPullParser.END_DOCUMENT) {
+            else if (parser.getEventType() != XmlPullParser.START_TAG && parser.next() != XmlPullParser.END_DOCUMENT) {
                 testItemsElems.questions = questions;
                 listTestItem.add(testItemsElems);
                 testItemsElems = new TestItems();
@@ -66,8 +68,6 @@ public class ParseXmlFile
                 String nameTage2 = parser.getName();
             }
         }
-        testItemsElems.questions = questions;
-        listTestItem.add(testItemsElems);
         return listTestItem;
     }
 
