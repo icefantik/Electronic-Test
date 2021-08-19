2f5040122daea6d76de4899bf66940e0ac60f419
petya <mityushin_petya@mail.ru>
2021-08-19 10:22:16 +0300

    start development count correct and not correct answer

--- app/src/main/java/com/example/electronictest/MainActivity.java
+++ app/src/main/java/com/example/electronictest/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
@@ -22,6 +23,9 @@ import javax.xml.parsers.ParserConfigurationException;
 public class MainActivity extends AppCompatActivity
 {
     private static int indexSetTests = 0;
+    private static int lengthSetTests = 0;
+    private static int countCorrectAnswer = 0;
+    private static int countNotCorrectAnswer = 0;
     private ArrayList<CheckBox> arrayCheckBoxs = new ArrayList<CheckBox>();
     private ArrayList<RadioButton> arrayRadioButton = new ArrayList<RadioButton>();
     private static ArrayList<ParseXmlFile.TestItems> setTests;
@@ -30,67 +34,111 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnToAnswer = new Button(this);
-        ImageView imageView = new ImageView(this);
-        TextView textViewQuestion = new TextView(this);
-
-        LinearLayout linearLayout = findViewById(R.id.rootContainer);
-        LinearLayout.LayoutParams paramsImage = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT),
-                paramsTextView = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT),
-                paramsButton = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        paramsButton.setMargins(0,0,0, 0);
-        paramsImage.setMargins(0, 100, 30, 0);
-        paramsTextView.setMargins(5, 0, 0, 50);
-
-        XmlPullParser parser = getResources().getXml(R.xml.test);
-        ParseXmlFile parseXmlFile = new ParseXmlFile();
-
-        try {
-            setTests = parseXmlFile.parsXmlFile(parser);
-        } catch (IOException | XmlPullParserException error) {
-            Toast.makeText(this, "" + error.toString(), Toast.LENGTH_LONG).show();
-            error.printStackTrace();
-        }
-
-        //если при чтении из xml файла указано налитиче картинти
-        btnToAnswer.setLayoutParams(paramsButton);
-        btnToAnswer.setText("To answer");
-
-        btnToAnswer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = getIntent();
-                overridePendingTransition(0, 0);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                finish();
-                ++indexSetTests;
-                overridePendingTransition(0, 0);
-                startActivity(intent);
+        if (indexSetTests == 0)
+        {
+            XmlPullParser parser = getResources().getXml(R.xml.test);
+            ParseXmlFile parseXmlFile = new ParseXmlFile();
+            try {
+                setTests = parseXmlFile.parsXmlFile(parser);
+                lengthSetTests = setTests.size();
+            } catch (IOException | XmlPullParserException error) {
+                Toast.makeText(this, "" + error.toString(), Toast.LENGTH_LONG).show();
+                error.printStackTrace();
             }
-        });
+        }
+        if (indexSetTests >= 0 && lengthSetTests > indexSetTests) {
+            Button btnToAnswer = new Button(this);
+            ImageView imageView = new ImageView(this);
+            TextView textViewQuestion = new TextView(this);
 
-        textViewQuestion.setText(setTests.get(indexSetTests).question);
-        textViewQuestion.setLayoutParams(paramsTextView);
-        linearLayout.addView(textViewQuestion);
-        /**
-         if (setTests.get(i).images != null) {
-         int id = getResources().getIdentifier("yourpackagename:drawable/" + setTests.get(i).images, null, null);
-         imageView.setImageResource(id);
-                imageView.setLayoutParams(paramsImage);
-         linearLayout.addView(imageView);
-         }*/
-        if (ParseXmlFile.KindCreateButton.RADIOBUTTON.ordinal() == setTests.get(indexSetTests).KindAnswer)
-        {
-            createRadioButtons(setTests.get(indexSetTests).answers);
+            LinearLayout linearLayout = findViewById(R.id.rootContainer);
+            LinearLayout.LayoutParams paramsImage = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT),
+                    paramsTextView = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT),
+                    paramsButton = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            paramsButton.setMargins(0, 0, 0, 0);
+            paramsImage.setMargins(0, 100, 30, 0);
+            paramsTextView.setMargins(5, 0, 0, 50);
+
+            //если при чтении из xml файла указано налитиче картинти
+            btnToAnswer.setLayoutParams(paramsButton);
+            //btnToAnswer.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL); //Gravity.CENTER_HORIZONTAL
+            btnToAnswer.setText("To answer");
+
+            btnToAnswer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = getIntent();
+                    overridePendingTransition(0, 0);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    finish();
+
+                    if (ParseXmlFile.KindCreateButton.RADIOBUTTON.ordinal() == setTests.get(indexSetTests).KindAnswer) {
+                        if (arrayRadioButton.get(setTests.get(indexSetTests).NumAnswer).isChecked()) {
+                            ++countCorrectAnswer;
+                        } else {
+                            ++countNotCorrectAnswer;
+                        }
+                    } else if (ParseXmlFile.KindCreateButton.CHECKBOX.ordinal() == setTests.get(indexSetTests).KindAnswer) {
+                        if (arrayCheckBoxs.get(setTests.get(indexSetTests).NumAnswer).isChecked()) {
+                            ++countCorrectAnswer;
+                        } else {
+                            ++countNotCorrectAnswer;
+                        }
+                    }
+
+                    //setTests.get(indexSetTests).;
+                    //setTests.get(indexSetTests).NumAnswer
+
+                    ++indexSetTests;
+                    overridePendingTransition(0, 0);
+                    startActivity(intent);
+                }
+            });
+
+            textViewQuestion.setText(setTests.get(indexSetTests).question);
+            textViewQuestion.setLayoutParams(paramsTextView);
+            linearLayout.addView(textViewQuestion);
+            /**
+             if (setTests.get(i).images != null) {
+             int id = getResources().getIdentifier("yourpackagename:drawable/" + setTests.get(i).images, null, null);
+             imageView.setImageResource(id);
+             imageView.setLayoutParams(paramsImage);
+             linearLayout.addView(imageView);
+             }*/
+            if (ParseXmlFile.KindCreateButton.RADIOBUTTON.ordinal() == setTests.get(indexSetTests).KindAnswer) {
+                createRadioButtons(setTests.get(indexSetTests).answers);
+            } else if (ParseXmlFile.KindCreateButton.CHECKBOX.ordinal() == setTests.get(indexSetTests).KindAnswer) {
+                createCheckBoxs(setTests.get(indexSetTests).answers);
+            }
+            linearLayout.addView(btnToAnswer);
         }
-        else if (ParseXmlFile.KindCreateButton.CHECKBOX.ordinal() == setTests.get(indexSetTests).KindAnswer)
-        {
-            createCheckBoxs(setTests.get(indexSetTests).answers);
+        else {
+            LinearLayout linearLayout = findViewById(R.id.rootContainer);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 0, 0, 0);
+
+            TextView correctAnswer = new TextView(this);
+            correctAnswer.setText("Count correct answer: " + countCorrectAnswer);
+            correctAnswer.setLayoutParams(layoutParams);
+            correctAnswer.setTextSize(25);
+            correctAnswer.setGravity(Gravity.CENTER_HORIZONTAL);
+
+            layoutParams.setMargins(0, 0, 0, 500);
+
+            TextView notCorrectAnswer = new TextView(this);
+            notCorrectAnswer.setText("Count not correct answer: " + countNotCorrectAnswer);
+            notCorrectAnswer.setLayoutParams(layoutParams);
+            notCorrectAnswer.setTextSize(25);
+            notCorrectAnswer.setGravity(Gravity.CENTER_HORIZONTAL);
+
+            linearLayout.addView(correctAnswer);
+            linearLayout.addView(notCorrectAnswer);
         }
-        linearLayout.addView(btnToAnswer);
     }
+
     private void createRadioButtons(ArrayList<String> answers)
     {
+
         LinearLayout linearLayout = findViewById(R.id.rootContainer);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.setMargins(0, 0, 30, 100);
@@ -108,6 +156,7 @@ public class MainActivity extends AppCompatActivity
     }
     private void createCheckBoxs(ArrayList<String> answers)
     {
+        //arrayCheckBoxs = new ArrayList<CheckBox>();
         LinearLayout linearLayout = findViewById(R.id.rootContainer);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.setMargins(0, 0, 30, 100);
